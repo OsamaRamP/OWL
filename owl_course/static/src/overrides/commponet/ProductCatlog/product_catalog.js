0b563e08644a0543a3f295e22fe68433293b6263
OsamaRamP <osama.ramadan.p@gmail.com>
2026-05-24 12:30:21 +0300

    [add] edit to session 4

--- owl_course/static/src/overrides/commponet/ProductCatlog/product_catalog.js
+++ owl_course/static/src/overrides/commponet/ProductCatlog/product_catalog.js
@@ -1,10 +1,10 @@
 /** @odoo-module **/
 
-import {Component,useState,useRef,useEffect, onWillStart, onMounted} from "@odoo/owl";
+import {Component,useState,useRef,useEffect, onWillStart, onMounted ,onWillUnmount } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { ProductCard } from "../ProductCard/product_card";
 import { CatalogHeader } from "../CatlogHeader/catalog_header";
-
+import { useService } from "@web/core/utils/hooks";
 
 export class ProductCatalog extends Component {
     static template = "owl_course.ProductCatalog";
@@ -12,20 +12,69 @@ export class ProductCatalog extends Component {
 
     setup() {
         this.searchInputRef = useRef("searchInput");
+        this.orm = useService("orm");
+        this.notification = useService("notification");
+        this.clearSearchB = useRef("clearSearchB");
         this.state = useState({
             search: "",
             cartCount: 0,
-            products:  [
-                { id: 1,image:"/owl_course/static/img/laptop.jpeg", name: "Laptop", price: 1500 },
-                { id: 2,image:"/owl_course/static/img/mouse.webp", name: "Mouse", price: 50 },
-                { id: 3,image:"/owl_course/static/img/keyboard.jpeg", name: "Keyboard", price: 100 },
-                { id: 4, image:"/owl_course/static/img/laptop.jpeg", name: "Monitor", price: 700 },
-            ]
+            products:  []
         });
+
+        onWillStart(async ()=>{
+                        console.log("this << onWillStart >> method");
+                   this.state.products = await  this.orm.searchRead('product.template' ,[] ,['id', 'name' , 'list_price'  ,'image_1920'] )
+//                   this.state.products =  this.orm.call('product.template', 'create',[])
+//                this.state.products =[
+//                        { id: 1,image:"/owl_course/static/img/laptop.jpeg", name: "Laptop", price: 1500 },
+//                        { id: 2,image:"/owl_course/static/img/mouse.webp", name: "Mouse", price: 50 },
+//                        { id: 3,image:"/owl_course/static/img/keyboard.jpeg", name: "Keyboard", price: 100 },
+//                        { id: 4, image:"/owl_course/static/img/laptop.jpeg", name: "Monitor", price: 700 },
+//                    ]
+
+        });
+
+        onMounted(() =>{
+                console.log("this << onMounted >> method");
+                this.searchInputRef.el.focus();
+
+        }
+
+        );
+
+        useEffect( ()=>{
+                        console.log("this << useEffect >> method");
+        },() =>[this.state.cartCount]
+
+        );
+        onWillUnmount(()=>{
+
+                                console.log("this << onWillUnmount >> method");
+
+        });
+
 
 
     }
 
+    get allFilterProduct(){
+               console.log("this << allFilterProduct >> method");
+            const search = this.state.search
+            if(!search){
+                        return this.state.products;
+            }
+            return this.state.products.filter( (product)=>
+                product.name.includes(search)
+            );
+
+    }
+
+    clearSearch(){
+      this.state.search = '';
+      this.searchInputRef.el.focus();
+//      this.clearSearchB.el.style.width = 100 +'px';
+//      this.clearSearchB.el.style= {high = 100 +'px';
+    }
 
 
     onSearchInput(ev) {
@@ -34,7 +83,11 @@ export class ProductCatalog extends Component {
 
 
 
-    addToCart() {
+    onMoreInfo() {
+        this.notification.add(
+         ("this product called."),
+                { type: "info" }
+        )
         this.state.cartCount++;
     }
 }
